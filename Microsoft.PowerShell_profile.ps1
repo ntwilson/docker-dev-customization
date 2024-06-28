29a7af71ad142d2a9fe211c37a9b617f8dae13c6
Nathan Wilson <nathan.wilson@ad.mea.energy>
2024-06-28 17:00:44 -0500

    working customization script

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+
+Import-Module posh-git
+
+oh-my-posh --init --shell pwsh --config ~/.config/oh-my-posh/ntwilson.omp.json | Invoke-Expression
+
+
+if (Test-Path .\.env) {
+  import-dotenv
+}
--- customize.sh
+++ customize.sh
@@ -3,12 +3,17 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
-git clone https://github.com/ntwilson/neovim-config.git ~/.config/nvim
+git clone https://github.com/ntwilson/neovim-config.git $HOME/.config/nvim
 
 
 # install oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
-mkdir -p ~/.config/oh-my-posh
+mkdir -p $HOME/.config/oh-my-posh
 cp ./ntwilson.omp.json ~/.config/oh-my-posh/ntwilson.omp.json
+mkdir $HOME/.config/powershell
+dos2unix ./Microsoft.PowerShell_profile.ps1
+cp ./Microsoft.PowerShell_profile.ps1 $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
 
-cat ./.bashrc >> ~/.bashrc
+dos2unix ./.bashrc
+cat ./.bashrc >> $HOME/.bashrc
+echo "done"
