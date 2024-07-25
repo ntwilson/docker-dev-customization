bd818ac4c815c1d05ce4f310dc5b80384e45952a
Nathan Wilson <ntwilson@gmail.com>
2024-07-25 16:00:50 -0500

    Update how we set personal environment variables

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,14 +2,11 @@ Import-Module posh-git
 
 oh-my-posh --init --shell pwsh --config ~/.config/oh-my-posh/ntwilson.omp.json | Invoke-Expression
 
-if (Test-Path .\.env) {
-  import-dotenv
+if (Test-Path /secrets/.env) {
+  import-dotenv /secrets/.env
 }
 
-$env:MSSQL03_SERVER = cat /secrets/MSSQL03_SERVER
-$env:MSSQL03_USER = cat /secrets/MSSQL03_USER
-$env:MSSQL03_PASSWORD = cat /secrets/MSSQL03_PASSWORD
+if (Test-Path /secrets/.ntw.env) {
+  import-dotenv /secrets/.ntw.env
+}
 
-$env:MSSQL04_SERVER = cat /secrets/MSSQL04_SERVER
-$env:MSSQL04_USER = cat /secrets/MSSQL04_USER
-$env:MSSQL04_PASSWORD = cat /secrets/MSSQL04_PASSWORD
