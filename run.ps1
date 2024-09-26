47ef59d6d23fa20253170e41097a1a87cc520175
Nathan Wilson <ntwilson@gmail.com>
2024-09-26 09:39:25 -0400

    Switch to regular docker volumes, since they're stored in private AppData

--- run.ps1
+++ run.ps1
@@ -23,34 +23,22 @@ function Create-Volume {
   }
 }
 
-Create-Volume "gitconfig-volume" "/gitconfigvolume"
-Create-Volume "dotnet-cache" "/usr/share/dotnet" 
-Create-Volume "dotnet" "/root/.dotnet" 
-Create-Volume "gh" "/root/.config/gh" 
-Create-Volume "paket" "/root/.config/Paket" 
-Create-Volume "nuget-config" "/root/.config/NuGet" 
-Create-Volume "nuget" "/root/.nuget" 
-Create-Volume "nuget-share" "/root/.local/share/NuGet" 
-Create-Volume "pdm" "/root/.local/share/pdm" 
-Create-Volume "nvim" "/root/.local/share/nvim"
-Create-Volume "powershell-history" "/root/.local/share/powershell/PSReadLine" 
-Create-Volume "az" "/root/.azure" 
-
 docker run -it `
   --rm `
-  --mount "type=bind,src=$home\DockerVolumes\workspace,dst=/workspace" `
-  --mount "type=bind,src=$home\DockerVolumes\gitconfig-volume,dst=/gitconfigvolume" `
-  --mount "type=bind,src=$home\DockerVolumes\dotnet-cache,dst=/usr/share/dotnet" `
-  --mount "type=bind,src=$home\DockerVolumes\dotnet,dst=/root/.dotnet" `
-  --mount "type=bind,src=$home\DockerVolumes\gh,dst=/root/.config/gh" `
-  --mount "type=bind,src=$home\DockerVolumes\paket,dst=/root/.config/Paket" `
-  --mount "type=bind,src=$home\DockerVolumes\nuget-config,dst=/root/.config/NuGet" `
-  --mount "type=bind,src=$home\DockerVolumes\nuget,dst=/root/.nuget" `
-  --mount "type=bind,src=$home\DockerVolumes\nuget-share,dst=/root/.local/share/NuGet" `
-  --mount "type=bind,src=$home\DockerVolumes\pdm,dst=/root/.local/share/pdm" `
-  --mount "type=bind,src=$home\DockerVolumes\nvim,dst=/root/.local/share/nvim" `
-  --mount "type=bind,src=$home\DockerVolumes\powershell-history,dst=/root/.local/share/powershell/PSReadLine" `
-  --mount "type=bind,src=$home\DockerVolumes\az,dst=/root/.azure" `
+  --mount "type=volume,src=workspace,dst=/workspace" `
+  --mount "type=volume,src=secrets,dst=/secrets" `
+  --mount "type=volume,src=gitconfig-volume,dst=/gitconfigvolume" `
+  --mount "type=volume,src=dotnet-cache,dst=/usr/share/dotnet" `
+  --mount "type=volume,src=dotnet,dst=/root/.dotnet" `
+  --mount "type=volume,src=gh,dst=/root/.config/gh" `
+  --mount "type=volume,src=paket,dst=/root/.config/Paket" `
+  --mount "type=volume,src=nuget-config,dst=/root/.config/NuGet" `
+  --mount "type=volume,src=nuget,dst=/root/.nuget" `
+  --mount "type=volume,src=nuget-share,dst=/root/.local/share/NuGet" `
+  --mount "type=volume,src=pdm,dst=/root/.local/share/pdm" `
+  --mount "type=volume,src=nvim,dst=/root/.local/share/nvim" `
+  --mount "type=volume,src=powershell-history,dst=/root/.local/share/powershell/PSReadLine" `
+  --mount "type=volume,src=az,dst=/root/.azure" `
   --mount "type=bind,src=$home\DockerClipBoard,dst=/clipboard" `
   @portArgs `
   $imageName $startCmd
