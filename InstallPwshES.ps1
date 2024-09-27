9c218e44bfd5f36c51f1ba74e985e4623aa78b11
Nathan Wilson <ntwilson@gmail.com>
2024-09-27 14:43:12 -0400

    fix the powershell editor service setup

--- InstallPwshES.ps1
+++ InstallPwshES.ps1
@@ -1,4 +1,7 @@
-cd ~
-gh release download -R PowerShell/PowerShellEditorServices -A zip -O .pwsh-es.zip
-Expand-Archive .pwsh-es.zip
+cd /root
+
+curl -OJL (curl -s https://api.github.com/repos/PowerShell/PowerShellEditorServices/releases/latest | grep browser_download_url | cut -d '"' -f 4)
+Expand-Archive PowerShellEditorServices.zip -DestinationPath .pwsh-es
+
+rm PowerShellEditorServices.zip
 
