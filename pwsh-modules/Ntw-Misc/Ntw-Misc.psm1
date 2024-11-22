ca5552755134d8ebd384692364302d2cebfc09f3
Nathan Wilson <ntwilson@gmail.com>
2024-11-22 09:09:58 -0500

    more capabilities to the mssql-cli script

--- pwsh-modules/Ntw-Misc/Ntw-Misc.psm1
+++ pwsh-modules/Ntw-Misc/Ntw-Misc.psm1
@@ -1,25 +1,45 @@
 function Mssql-Cli {
-  param ([String][Parameter(Mandatory=$True)]$Server, [string][Parameter(Mandatory=$True)]$Database, [string][Parameter(Mandatory=$True)]$Query)
+  param (
+    [String][Parameter(Mandatory=$True)]$Server,
+    [string][Parameter(Mandatory=$True)]$Database,
+    [string]$Query,
+    [string]$InputScript,
+    [string]$Variable
+  )
 
   if ($Server -match "mssql03") {
     $Server = Get-azKeyVaultSecret -VaultName MeaOnPrem -Name MSSQL03-SERVER -AsPlainText
     $user = Get-azKeyVaultSecret -VaultName MeaOnPrem -Name MSSQL03-USER -AsPlainText
     $pass = Get-azKeyVaultSecret -VaultName MeaOnPrem -Name MSSQL03-PASSWORD -AsPlainText
-    $arguments = @{ ServerInstance = $Server; Username = $user; Password = $pass; Database = $Database; Query = $Query; TrustServerCertificate = $true }
+    $arguments = @{ ServerInstance = $Server; Username = $user; Password = $pass; Database = $Database; TrustServerCertificate = $true }
   }
   elseif ($Server -match "mssql04") {
     $Server = Get-azKeyVaultSecret -VaultName MeaOnPrem -Name MSSQL04-SERVER -AsPlainText
     $user = Get-azKeyVaultSecret -VaultName MeaOnPrem -Name MSSQL04-USER -AsPlainText
     $pass = Get-azKeyVaultSecret -VaultName MeaOnPrem -Name MSSQL04-PASSWORD -AsPlainText
-    $arguments = @{ ServerInstance = $Server; Username = $user; Password = $pass; Database = $Database; Query = $Query; TrustServerCertificate = $true }
+    $arguments = @{ ServerInstance = $Server; Username = $user; Password = $pass; Database = $Database; TrustServerCertificate = $true }
   }
   elseif ($Server -match "azure") {
     $token = (Get-AzAccessToken -ResourceUrl https://database.windows.net -AsSecureString).Token | ConvertFrom-SecureString -AsPlainText
-    $arguments = @{ ServerInstance = $env:DB_SERVER; Database = $Database; AccessToken = $token; Query = $Query }
+    $arguments = @{ ServerInstance = $env:DB_SERVER; Database = $Database; AccessToken = $token; }
   }
   else {
     $token = (Get-AzAccessToken -ResourceUrl https://database.windows.net -AsSecureString).Token | ConvertFrom-SecureString -AsPlainText
-    $arguments = @{ ServerInstance = $Server; Database = $Database; AccessToken = $token; Query = $Query }
+    $arguments = @{ ServerInstance = $Server; Database = $Database; AccessToken = $token; }
+  }
+
+  if ($Query) {
+    $arguments.Add("Query", $Query)
+  }
+  elseif ($InputScript) {
+    $arguments.Add("InputFile", $InputScript)
+  }
+  else {
+    Write-Error "Must have either a Query or an InputScript provided"
+  }
+
+  if ($Variable) {
+    $arguments.Add("Variable", $Variable)
   }
 
   Invoke-SqlCmd @arguments
