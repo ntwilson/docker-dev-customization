13d9933cee7d11370a98d5280cee5f522c02db89
Nathan Wilson <ntwilson@gmail.com>
2025-02-19 12:24:30 -0500

    formatting of the azure vm setup

--- AzureVMSetup.bash
+++ AzureVMSetup.bash
@@ -30,27 +30,27 @@ docker tag "meacontainers.azurecr.io/dev-env-ntw:latest" dev-env-ntw
 mkdir ~/DockerClipBoard
 
 cat > run.bash <<EOF 
-docker run -it \
-  --rm \
-  --mount "type=volume,src=workspace,dst=/workspace" \
-  --mount "type=volume,src=gitconfig-volume,dst=/gitconfigvolume" \
-  --mount "type=volume,src=dotnet-cache,dst=/usr/share/dotnet" \
-  --mount "type=volume,src=dotnet,dst=/root/.dotnet" \
-  --mount "type=volume,src=gh,dst=/root/.config/gh" \
-  --mount "type=volume,src=paket,dst=/root/.config/Paket" \
-  --mount "type=volume,src=nuget-config,dst=/root/.config/NuGet" \
-  --mount "type=volume,src=nuget,dst=/root/.nuget" \
-  --mount "type=volume,src=nuget-share,dst=/root/.local/share/NuGet" \
-  --mount "type=volume,src=pdm,dst=/root/.local/share/pdm" \
-  --mount "type=volume,src=powershell-history,dst=/root/.local/share/powershell/PSReadLine" \
-  --mount "type=volume,src=az,dst=/root/.azure" \
-  --mount "type=volume,src=az-pwsh,dst=/root/.Azure" \
-  --mount "type=volume,src=azcache,dst=/root/.local/share/.IdentityService" \
-  --mount "type=bind,src=\$HOME/DockerClipBoard,dst=/clipboard" \
-  -p "1080:1080" \
-  --cap-add NET_ADMIN \
-  --sysctl net.ipv6.conf.all.disable_ipv6=0 \
-  --sysctl net.ipv4.conf.all.src_valid_mark=1 \
+docker run -it \\
+  --rm \\
+  --mount "type=volume,src=workspace,dst=/workspace" \\
+  --mount "type=volume,src=gitconfig-volume,dst=/gitconfigvolume" \\
+  --mount "type=volume,src=dotnet-cache,dst=/usr/share/dotnet" \\
+  --mount "type=volume,src=dotnet,dst=/root/.dotnet" \\
+  --mount "type=volume,src=gh,dst=/root/.config/gh" \\
+  --mount "type=volume,src=paket,dst=/root/.config/Paket" \\
+  --mount "type=volume,src=nuget-config,dst=/root/.config/NuGet" \\
+  --mount "type=volume,src=nuget,dst=/root/.nuget" \\
+  --mount "type=volume,src=nuget-share,dst=/root/.local/share/NuGet" \\
+  --mount "type=volume,src=pdm,dst=/root/.local/share/pdm" \\
+  --mount "type=volume,src=powershell-history,dst=/root/.local/share/powershell/PSReadLine" \\
+  --mount "type=volume,src=az,dst=/root/.azure" \\
+  --mount "type=volume,src=az-pwsh,dst=/root/.Azure" \\
+  --mount "type=volume,src=azcache,dst=/root/.local/share/.IdentityService" \\
+  --mount "type=bind,src=\\$HOME/DockerClipBoard,dst=/clipboard" \
+  -p "1080:1080" \\
+  --cap-add NET_ADMIN \\
+  --sysctl net.ipv6.conf.all.disable_ipv6=0 \\
+  --sysctl net.ipv4.conf.all.src_valid_mark=1 \\
   dev-env-ntw pwsh
 EOF
 
